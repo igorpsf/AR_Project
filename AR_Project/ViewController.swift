3d465c3cb85b5aa70e643f4971637f15a68905b5
Igor Postnikov <igor.postnikov8@gmail.com>
2021-02-14 21:28:24 -0800

    added pictures and prepared api

--- AR_Project/ViewController.swift
+++ AR_Project/ViewController.swift
@@ -11,9 +11,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = "https://api.exchangeratesapi.io/latest?base=GBP&symbols=USD"
+        getData(from: url)
     }
-
-
+    
+    private func getData(from url: String){
+        
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            } catch {
+                print("failed to convert \(error.localizedDescription)")
+            }
+            
+            guard let json = result else {
+                return
+            }
+            
+            print(json.rates.USD)
+        })
+        
+        task.resume()
+        
+        
+    }
 }
 
+
+struct Response: Codable {
+    //let results: MyResult
+    let rates: MyResult
+    let base: String
+    let date: String
+}
+
+struct MyResult: Codable {
+    let USD: Int
+}
